5cc074f312813432f0b44890fdefe4735f316272
Christopher Reitz <cre@christohersmini.fritz.box>
2014-07-28 19:49:08 +0200

    Implemented html5 history api

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function($) {
+    // Main page elements
     var $container = $('.layout');
     var $north     = $('.north');
     var $center    = $('.center');
@@ -22,85 +23,137 @@ $(function($) {
         stop: relayout
     });
 
-    // pxmboard foo
-    var $northContent = $('<div/>').addClass('content');
-    var $southContent = $('<div/>').addClass('content');
+
+    // PXMBoard foo
+    var curMsgid, curThrdid;
+
+    var $northContent  = $('<div/>').addClass('content');
+    var $centerContent = $('<div/>').addClass('content');
+    var $southContent  = $('<div/>').addClass('content');
 
     $north.append($northContent);
+    $center.append($centerContent);
     $south.append($southContent);
 
-    var loadMsg = function (event, msgid) {
-        event.preventDefault();
-        $southContent.load("content/msg/" + msgid + ".html", {}, function () {
-            $south.find("a").each(function () {
-                var $a   = $(this);
-                var href = $a.attr('href');     
-                
-                switch (true) {
-                    case href.search(/^pxmboard.php\?mode=message&/) !== -1:
-                        var msgid = href.replace("pxmboard.php?mode=message&brdid=27&msgid=", "");
-                        $a.click(function (event) {
-                            loadMsg(event, msgid);
-                            cp('p' + msgid);
-
-                            return false;
-                        });
-                        break;
-
-                    case href.search(/pxmboard.php\?mode=userprofile&/) !== -1:
-                        $a.attr("onmouseout", "").attr("onmouseover", "");
-                        break;
-                }
-            });
-        });
+    var getQueryStringParameterByName = function (name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     };
 
-    var loadThread = function (event, thrdid) {
-        event.preventDefault();
-        $center.load("content/thread/" + thrdid + ".html", {}, function () {
-            $center.find("a").each(function () {
-                var $a = $(this);
-                $a.click(function (event) {
-                    var msgid = /\d+/.exec($a.attr("onclick"))[0];
-                    loadMsg(event, msgid);
+    var changeUrl = function (thrdid, msgid) {
+        var url = [];
+        
+        if (msgid !== undefined) {
+            url.push("thrdid=" + thrdid);
+        }        
+        if (msgid !== undefined) {
+            url.push("msgid=" + msgid);
+        }
+        
+        if (url.length > 0) {
+            url = "?" + url.join("&");
+            History.pushState(null, document.title, url);
+        }
+    };
 
-                    return false;
+    var loadMsg = function (msgid) {
+        if (msgid === curMsgid) {
+            return;
+        }
+
+        if (msgid.length === 0) {
+            $southContent.empty();
+        } else {
+            $southContent.load("content/msg/" + msgid + ".html", {}, function () {
+                curMsgid = msgid;
+                if (typeof cp === "function") {
+                    cp('p' + msgid);
+                }
+                $south.find("a").each(function () {
+                    var $a   = $(this);
+                    var href = $a.attr('href');     
+                    
+                    switch (true) {
+                        case href.search(/^pxmboard.php\?mode=message&/) !== -1:
+                            var msgid = href.replace("pxmboard.php?mode=message&brdid=27&msgid=", "");
+                            $a.click(function (event) {
+                                loadMsg(msgid);
+                                changeUrl(getQueryStringParameterByName("thrdid"), msgid);
+                                event.preventDefault();
+                            });
+                            break;
+
+                        case href.search(/pxmboard.php\?mode=userprofile&/) !== -1:
+                            $a.attr("onmouseout", "").attr("onmouseover", "");
+                            break;
+                    }
                 });
             });
-        });
+        }
+    };
+
+    var loadThread = function (thrdid) {
+        if (thrdid === curThrdid) {
+            return;
+        }
+
+        if (thrdid.length === 0) {
+            $centerContent.empty();
+        } else {
+            $centerContent.load("content/thread/" + thrdid + ".html", {}, function () {
+                curThrdid = thrdid;
+                $centerContent.find("a").each(function () {
+                    var $a = $(this);
+                    $a.click(function (event) {
+                        var msgid = /\d+/.exec($a.attr("onclick"))[0];
+                        loadMsg(msgid);
+                        changeUrl(thrdid, msgid);
+                        event.preventDefault();
+                    });
+                });
+            });
+        }
+    };
+
+    var statechange = function () {
+        loadThread(getQueryStringParameterByName("thrdid"));
+        loadMsg(getQueryStringParameterByName("msgid"));
     };
 
     $northContent.load("content/threadlist.html", null, function () {
-
         var $header = $('#header');
         $($header.parents('table').find('tr').get(1)).remove();
         $header
             .removeAttr('valign').removeAttr('align')
             .append('<img style="float: left; margin-right: 10px;" src="images/trophy.png">')
-            .append('<p style="margin: 0px; font-size: 16px;">man!ac WM-Forum 2014 Archiv</p>')
+            .append('<p style="margin: 0px; font-size: 16px;">' + document.title + '</p>')
         ;
 
         $north.find("a[target='middle']").each(function () {
             var $a = $(this);
             $a.click(function (event) {
                 var thrdid = $a.attr("href").replace("pxmboard.php?mode=thread&brdid=27&thrdid=", "");
-                loadThread(event, thrdid);              
-
-                return false;
+                loadThread(thrdid);             
+                changeUrl(thrdid); 
+                event.preventDefault();
             });
             
         });
         $north.find("a[target='bottom']").each(function () {
             var $a = $(this);
-
             $a.click(function (event) {
                 var thrdid = /\d+/.exec($a.attr("onclick"))[0];
                 var msgid  = $a.attr("href").replace("pxmboard.php?mode=message&brdid=27&msgid=", "");
-                loadThread(event, thrdid);
-                loadMsg(event, msgid);
-
-                return false;
+                loadThread(thrdid);
+                loadMsg(msgid);
+                changeUrl(thrdid, msgid);
+                event.preventDefault();
             });         
         });
     });
+
+    History.Adapter.bind(window, "statechange", statechange);
+    statechange();
 });
